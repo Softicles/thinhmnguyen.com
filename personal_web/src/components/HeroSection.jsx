ec45ba1ac4aa6c79451e6e15b6ab2671dc98bf80
Softicles <thinhmnguyen4105@gmail.com>
2025-09-16 00:41:20 -0400

    update resume

--- personal_web/src/components/HeroSection.jsx
+++ personal_web/src/components/HeroSection.jsx
@@ -76,7 +76,7 @@ export const HeroSection = () => {
 
                             <br />
                             <a
-                                href="https://drive.google.com/file/d/13Arlfj3renqCSCVsivgwEMj9-mBmEWWv/view?usp=sharing"
+                                href="https://drive.google.com/file/d/1NkSlt955-5VJYKMvZkFufyK9HSUnN37r/view?usp=sharing"
                                 className="px-3 py-1 text-[19px] rounded-full border border-primary font-bold text-primary hover:bg-primary/10 transition-colors duration-300"
                                 target="_blank"
                             >
